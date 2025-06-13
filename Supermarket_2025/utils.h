b164cdd8821bd08d36fb01cf70c51cf5639ce03e
BuildTools <unconfigured@null.spigotmc.org>
2025-06-13 15:56:36 +0300

    utils.h

--- Supermarket_2025/utils.h
+++ Supermarket_2025/utils.h
@@ -1,1 +1,20 @@
 #pragma once
+
+void strcpy(char* dest, const char* src, int maxSize);
+
+void strCompareNoCase(const char* str1, const char* str2);
+
+void trimString(char* str);
+
+bool isValidNUmber(const char* str);
+
+void getCurrentDate(char* dateStr, int maxSize);
+
+void formatDate(char* foramtDate, int maxSize);
+
+bool isValidEmail(const char* email);
+
+bool isValidPassword(const char* password);
+
+bool isValidPrice(const char* priceStr);
+
